7e35590794d4a52109b0ab26d68099aec1aa5c5f
Michael Josephson <mikej@users.noreply.github.com>
2022-08-06 13:59:25 +0100

    include URL in Result
    makes URL from after any redirects accessible to the caller

--- lib/rfetch.rb
+++ lib/rfetch.rb
@@ -10,7 +10,7 @@ require "faraday"
 module RFetch
   REDIRECT_CODES  = Set.new [301, 302, 303, 307, 308]
 
-  Result = Struct.new(:status_code, :content_type, :body) do
+  Result = Struct.new(:url, :status_code, :content_type, :body) do
     def to_page
       if content_type == "text/html"
         Page.new(body)
@@ -23,11 +23,11 @@ module RFetch
   def self.get(url_requested)
     conn = Faraday::Connection.new
 
-    resp = following_redirects(url_requested) do |url|
+    url, resp = following_redirects(url_requested) do |url|
       conn.get(url) { |req| req.options.timeout = 5 }
     end
 
-    Result.new(resp.status, resp.headers["content-type"], resp.body)
+    Result.new(url, resp.status, resp.headers["content-type"], resp.body)
   end
 
   private_class_method def self.following_redirects(url)
@@ -37,7 +37,7 @@ module RFetch
       resp = yield url
       seen << url
 
-      return resp unless REDIRECT_CODES.include?(resp.status)
+      return [url, resp] unless REDIRECT_CODES.include?(resp.status)
 
       redirect = build_redirect(url, resp)
 
