2b557df9c6f0ad10d11b11a7d18713d180c9e683
Michael Josephson <mikej@users.noreply.github.com>
2022-08-06 09:49:58 +0100

    remove failing example spec

--- spec/rfetch_spec.rb
+++ spec/rfetch_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RFetch do
   it "has a version number" do
     expect(RFetch::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
